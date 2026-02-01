5bb480bef2df774d4e4c020d6a4a3a9e992aaa34
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-01 23:46:43 +0700

    init commit

---
+++ Pictures/Program.cs
@@ -0,0 +1,12 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var picturesInRow = 3;
+        var totalPictures = 52;
+        var fullRows = totalPictures / picturesInRow;
+        var extraPictures = totalPictures % picturesInRow;
+        Console.WriteLine($"Полных рядов по {picturesInRow} картинки: {fullRows}");
+        Console.WriteLine($"Лишних картинок: {extraPictures}");
+    }
+}
