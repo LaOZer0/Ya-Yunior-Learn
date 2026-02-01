96181ab0935ac4e9ef37ee790d02bd1592f340b7
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-01 22:28:23 +0700

    init commit

---
+++ Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿public static class Program
+{
+    static void Main(string[] args)
+    {
+        bool isSomething = true;
+        int integerNumber = 10;
+        float floatNumber = 5F;
+        uint unsignedIntegerNumber = 10;
+        string name = "Oleg";
+        double doubleNumber = 5d;
+        char letter = 'a';
+        char anotherLetter = (char)(letter + 1);
+        char[] letters = { 'a', 'b', 'c' };
+        object[] objects = { isSomething, integerNumber, floatNumber, unsignedIntegerNumber, name, doubleNumber, letter, anotherLetter };
+        foreach (var obj in objects)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
