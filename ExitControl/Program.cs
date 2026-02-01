98eb18172b08fba857cb3260ab79e334d1fc4fad
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-02 03:15:09 +0700

    init commit

---
+++ ExitControl/Program.cs
@@ -0,0 +1,13 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var exitMessage = "exit";
+        var playerInput = "";
+        while (playerInput != exitMessage)
+        {
+            Console.Write("Введите слово: ");
+            playerInput = Console.ReadLine();
+        }
+    }
+}
