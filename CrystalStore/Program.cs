e7518b69ea4e4cef36cf0787878be9c60ee44eba
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-02 02:32:06 +0700

    init commit

---
+++ CrystalStore/Program.cs
@@ -0,0 +1,27 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var objectCountMin = 0;
+        
+        Console.Write("Введите начальное кол-во золота: ");
+
+        if (int.TryParse(Console.ReadLine(), out var playerGoldCount) == false || playerGoldCount < objectCountMin)
+        {
+            Console.WriteLine($"Кол-во золота должно быть числом больше {objectCountMin}");
+            return;
+        }
+
+        var crystalPrice = 10;
+        Console.Write($"Введите кол-во кристаллов, которое вы хотите купить. Цена {crystalPrice}: ");
+
+        if (int.TryParse(Console.ReadLine(), out var boughtCrystalCount ) == false || boughtCrystalCount < objectCountMin)
+        {
+            Console.WriteLine($"Кол-во кристаллов должно быть числом больше {objectCountMin}");
+            return;
+        }
+
+        playerGoldCount = Math.Max(playerGoldCount - boughtCrystalCount * crystalPrice, objectCountMin);
+        Console.WriteLine($"Остаток золота: {playerGoldCount} \nКуплено кристаллов: {boughtCrystalCount}");
+    }
+}
