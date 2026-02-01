cb87c6cff4798eede41f0f3fcf0eac7d173162b3
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-01 23:56:14 +0700

    init commit

---
+++ Swap/Program.cs
@@ -0,0 +1,18 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var firstValue = 1;
+        var secondValue = 2;
+        Console.WriteLine($"{firstValue}, {secondValue}");
+        Swap(ref firstValue, ref secondValue);
+        Console.WriteLine($"{firstValue}, {secondValue}");
+    }
+    
+    private static void Swap<T>(ref T firstValue, ref T secondValue)
+    {
+        T temp = firstValue;
+        firstValue = secondValue;
+        secondValue = temp;
+    }
+}
