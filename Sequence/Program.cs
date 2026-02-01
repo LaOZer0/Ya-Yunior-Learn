6854ca72d686c29b94a62e8229e6565d2034daf7
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-02 03:46:43 +0700

    init commit

---
+++ Sequence/Program.cs
@@ -0,0 +1,15 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var step = 7;
+        var lastNumber = 103;
+        var currentNumber = 5;
+
+        while (currentNumber <= lastNumber)
+        {
+            Console.WriteLine(currentNumber);
+            currentNumber += step;
+        }
+    }
+}
