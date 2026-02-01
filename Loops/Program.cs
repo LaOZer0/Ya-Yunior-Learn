0bf24cbef9005ea4069aaee09ed7735081d8a781
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-02 03:11:17 +0700

    init commit

---
+++ Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите сообщение для повтора: ");
+        var message = Console.ReadLine();
+        Console.Write("Введите кол-во повторов сообщения: ");
+        
+        if (int.TryParse(Console.ReadLine(), out var countRepeat) == false || countRepeat < 0)
+        {
+            Console.WriteLine("Кол-во повторов должно быть число большее 0");
+        }
+
+        for (var i = 0; i < countRepeat; i++)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
