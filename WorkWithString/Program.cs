b298dcf3c0074c942a22f1f3dc6c901503a25721
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-01 23:06:59 +0700

    Task1_3_String - вынес мин и макс возраст в константы, добавил метод проверки возраста, заменил ! на false (очень с этим не согласен)

--- WorkWithString/Program.cs
+++ WorkWithString/Program.cs
@@ -1,19 +1,29 @@
 ﻿public static class Program
 {
+    private const int MIN_AGE = 0;
+    private const int MAX_AGE = 130;
+
     public static void Main(string[] args)
     {
         Console.Write("Введите ваше имя: ");
         var name = Console.ReadLine();
-        Console.Write("Введите ваш возраст от 0 до 130: ");
-        if (!Int32.TryParse(Console.ReadLine(), out var age) || age < 0 || age > 130)
+        Console.Write($"Введите ваш возраст от {MIN_AGE} до {MAX_AGE}: ");
+
+        if (int.TryParse(Console.ReadLine(), out var age) == false || CheckAge(age))
         {
-            Console.WriteLine("Возраст должен быть положительным числом от 0 до 130");
+            Console.WriteLine($"Возраст должен быть положительным числом от {MIN_AGE} до {MAX_AGE}");
             return;
         }
+
         Console.Write("Введите ваш знак зодиака: ");
         var zodiac = Console.ReadLine();
         Console.Write("Введите вашу профессию: ");
         var job = Console.ReadLine();
         Console.WriteLine($"Вас зовут {name}, ваш возраст {age}, ваш знак зодиака {zodiac}, ваша профессия {job}");
+    }
+
+    private static bool CheckAge (int age)
+    {
+        return age >= MIN_AGE && age <= MAX_AGE;
     }
 }
