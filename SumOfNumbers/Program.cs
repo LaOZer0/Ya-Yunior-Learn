abe18ac36d009d9341ced4ec0e3497c4cd6862fd
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-02 20:35:42 +0700

    init commit

---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var number = new Random().Next(0, 101);
+        var sumNumbers = 0;
+        var multipleOfThree = 3;
+        var multipleOfFive = 5;
+
+        for (var i = 1; i * multipleOfFive < number && i * multipleOfThree < number;  i++)
+        {
+            sumNumbers += i * multipleOfThree + i * multipleOfFive;
+        }
+
+        Console.WriteLine(sumNumbers);
+    }
+}
