74afd502ab1a368a2d8edf9b17789f3fe740e0af
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-03 23:02:10 +0700

    init commit

---
+++ OutputName/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+public static class Program
+{
+
+
+    public static void Main(string[] args)
+    {
+        int countExtraSymbol = 2;
+        Console.WriteLine("Введите ваше имя");
+        var name = Console.ReadLine();
+        Console.WriteLine("Введите символ");
+        int totalLength = name.Length + countExtraSymbol;
+        var symbols = new StringBuilder(totalLength).Insert(0, Console.ReadLine(), totalLength).ToString();
+        
+        for (var i = 0; i < 3; i++)
+        {
+            if (i == 1)
+            {
+                Console.WriteLine(symbols.Remove(1, name.Length).Insert(1, name));
+                continue;
+            }
+
+            Console.WriteLine(symbols);
+        }
+    }
+}
