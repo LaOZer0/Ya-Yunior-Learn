b5079d1f0d9fe15b553148685cebdd14efc2df75
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-03 23:20:19 +0700

    init commit

---
+++ Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        int beginningOfRange = 10;
+        int endOfRange = 25;
+        int randomDivider = new Random().Next(beginningOfRange, endOfRange + 1);
+
+        beginningOfRange = 50;
+        endOfRange = 150;
+        int countOfMultiples = 0;
+
+        Console.WriteLine($"Ваш делитель: {randomDivider}");
+
+        for (var i = 0; i < endOfRange; i += randomDivider)
+        {
+            if (i > beginningOfRange)
+            {
+                countOfMultiples += 1;
+            }
+        }
+
+        Console.WriteLine(countOfMultiples);
+    }
+}
