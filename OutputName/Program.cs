7720978958e82337ca6930a2a561bbe24da12f58
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-03 23:45:45 +0700

    fix

--- OutputName/Program.cs
+++ OutputName/Program.cs
@@ -2,26 +2,16 @@
 
 public static class Program
 {
-
-
     public static void Main(string[] args)
     {
-        int countExtraSymbol = 2;
-        Console.WriteLine("Введите ваше имя");
-        var name = Console.ReadLine();
         Console.WriteLine("Введите символ");
-        int totalLength = name.Length + countExtraSymbol;
-        var symbols = new StringBuilder(totalLength).Insert(0, Console.ReadLine(), totalLength).ToString();
-        
-        for (var i = 0; i < 3; i++)
-        {
-            if (i == 1)
-            {
-                Console.WriteLine(symbols.Remove(1, name.Length).Insert(1, name));
-                continue;
-            }
+        string symbol = Console.ReadLine();
+        Console.WriteLine("Введите ваше имя");
+        var nameWithFrame = $"{symbol}{Console.ReadLine()}{symbol}";
+        var symbolsLine = new StringBuilder(nameWithFrame.Length).Insert(0, symbol, nameWithFrame.Length).ToString();
 
-            Console.WriteLine(symbols);
-        }
+        Console.WriteLine(symbolsLine);
+        Console.WriteLine(nameWithFrame);
+        Console.WriteLine(symbolsLine);
     }
 }
