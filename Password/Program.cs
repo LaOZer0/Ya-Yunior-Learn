723c8728eb82ffd9ffbb159e45867cfa5f8834be
LaOZer0 <74765201+LaOZer0@users.noreply.github.com>
2026-02-03 23:10:46 +0700

    init commit

---
+++ Password/Program.cs
@@ -0,0 +1,23 @@
+﻿public static class Program
+{
+    public static void Main(string[] args)
+    {
+        int countTries = 3;
+        string password = "password";
+        string secretMessage = "message";
+        Console.WriteLine("Введите пароль для доступа");
+        string userInput;
+
+        for (var i = 0; i < countTries; i++)
+        {
+            userInput = Console.ReadLine();
+            if (userInput == password)
+            {
+                Console.WriteLine(secretMessage);
+                continue;
+            }
+
+            Console.WriteLine("Попробуйте ещё раз");
+        }
+    }
+}
